1ed026df99a707c8610c6daeaf8ade5f1b15765b
Kelvin Chappell <kelvin.chappell@guardian.co.uk>
2013-01-16 09:33:21 +0000

    Solved single section

--- src/main/scala/s/S.scala
+++ src/main/scala/s/S.scala
@@ -1,25 +1,40 @@
 package s
 
 object Client extends App {
-  val x = new Section(List(1, 2, 3, 4, 5, 6, 7, 8, 0))
-  println(x.solve)
-  val y = new Section(List(1, 2, 3, 4, 5, 6, 7, 0, 0))
-  println(y.solve)
+  val x = new Section().solve(List(1, 2, 3, 4, 5, 6, 7, 8, 0))
+  val y = new Section().solve(List(1, 2, 3, 4, 5, 6, 7, 0, 0))
 }
 
-class Section(values: List[Int]) {
+class Section {
 
-  def solve: Section = {
-    val p = List(1, 2, 3, 4, 5, 6, 7, 8, 9)
-    val ps = List(p,p,p,p,p,p,p,p,p)
-    new Section(ps)
+  val p = Seq(1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+  def solve(values: List[Int]): Section = {
+    val ps = List(p, p, p, p, p, p, p, p, p)
+    val r = replace(values)
+    val s = is(r)
+    val x = f(r, s)
+    println(x)
+    new Section
   }
 
-  def f(y:List[Any]) = y.map { i => i match {
-  | case v:Int => v
-    | case s:Seq[Int] => s.filterNot(x => is.contains(x))
-    | }
-  | }
+  def f(y: List[Any], known: List[Int]) = y.map {
+    i => i match {
+      case v: Int => v
+      case s: Seq[Int] => {
+        val m = s.filterNot(x => known.contains(x))
+        if (m.length == 1) m.head else m
+      }
+    }
+  }
 
-  val is = o.collect { case i:Int => i}
+  def is(values: List[Any]) = values.collect {
+    case i: Int => i
+  }
+
+  def replace(x: List[Int]): List[Any] = x.map(y => y match {
+    case i if i > 0 => i
+    case _ => p
+  })
+
 }
